de26711f8e48cc7d6d298c0f94e84792a9e824a4
Nafiz-Anam <nafiz.anam002@gmail.com>
2022-12-18 00:30:27 +0600

    trying to add header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,12 @@
-import './globals.css'
-import Navbar from '../components/Header/Navbar'
-export default function Layout({children}: {children: React.ReactNode}) {
-  return (
-    <>  
-       <Navbar/>
-    {children} 
-    </>
-  )
+import "./globals.css";
+import Navbar from "../components/Header/Navbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <Navbar/>
+
+            {children}
+        </>
+    );
 }
